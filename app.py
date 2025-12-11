aec5640add21ddc74b73faac583eb418364636e6
zayennn <zaayeennn@gmail.com>
2025-12-11 23:46:25 +0700

    setup database

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# setup database
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "db_todo_list"
 
 @app.route('/')
 def index() :
